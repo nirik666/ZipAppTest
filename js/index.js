efc005e6a6fe46952d2a6c54d924ef73020b30c5
nirik666 <a@a>
2016-09-16 18:56:30 +0900

    debug out

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 document.addEventListener('deviceready', function(){
-
+    /*
+    var ftrans = new FileTransfer();
+    ftrans.download(
+        encodeURI("https://nirik666.github.io/ZipAppTest/test.html"),
+    );
+    */
+    window.alert(cordova.file.dataDirectory);
 }, false);
