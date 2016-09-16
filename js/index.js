5d023050146477a19065ba03d811728a46c489d2
nirik666 <a@a>
2016-09-16 19:05:23 +0900

    dl

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 document.addEventListener('deviceready', function(){
-    /*
     var ftrans = new FileTransfer();
     ftrans.download(
         encodeURI("https://nirik666.github.io/ZipAppTest/test.html"),
+        cordova.file.dataDirectory + "test.html",
+        function(){
+            window.alert("DL success");
+            window.location.href = cordova.file.dataDirectory + "test.html";
+        },
+        function(){
+            window.alert("DL error");
+        }
     );
-    */
-    window.alert(cordova.file.dataDirectory);
 }, false);
